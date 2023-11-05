c3dfd9aa56d9634ab339a9c5f41249e78f9d477f
Kairu-stonks <149807909+Kairu-stonks@users.noreply.github.com>
2023-11-05 11:32:09 +0800

    Update and rename README.md to script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+document.getElementById('name').addEventListener('input', function(e) {
+    e.target.style.borderColor = '';
+});
+
+document.getElementById('email').addEventListener('input', function(e) {
+    e.target.style.borderColor = '';
+});
+
+document.getElementById('message').addEventListener('input', function(e) {
+    e.target.style.borderColor = '';
+});
+
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    var name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var message = document.getElementById('message');
+
+    if(name.value.trim() === '') {
+        name.style.borderColor = 'red';
+        return;
+    }
+
+    if(email.value.trim() === '') {
+        email.style.borderColor = 'red';
+        return;
+    }
+
+    if(message.value.trim() === '') {
+        message.style.borderColor = 'red';
+        return;
+    }
+
+    alert('Message sent successfully!');
+});
